be531f3f5504ec38b65a061074624c39479539b4
viratlogu1832gmail.com <viratlogu183@gmail.com>
2024-03-26 17:41:54 +0530

    adding the context task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import "./App.css";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "./Components/Card";
+import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <><div className="fixed-top">
+      <Navbar/>
+      <Header />
+      </div>
+      <div className="d-flex flex-column justify-content-center align-items-center cardcss">
+      <Card  />
+      </div>
+      <Footer/>
+    </>
+  );
 }
 
 export default App;
